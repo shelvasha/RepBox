8044dcdd16bdf9f32e26141f8dd6ffb120d0ff61
Shel Burkes <shel.burkes@gmail.com>
2020-05-06 16:34:11 -0400

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -7,6 +7,12 @@ read input
 if [ $input = "Y" ]; then
     rm -rf $(ls -d *_out)
     rm $(ls genome/*.fai)
+#    rm $(find . -type f -regex ".*\.bt2")
+#    rm $(find . -type f -regex ".*\.tmap")
+#    rm $(find . -type f -regex ".*\.bam")
+#    rm $(find . -type f -regex ".*\.sam")
+#    rm $(find . -type f -regex ".*\.rmdup.bed")
+
     echo "Repbox directory reset."
 
 else

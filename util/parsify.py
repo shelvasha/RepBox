e9072f6770b2b9a06666857cfc1bfd769b59ff7a
Shel Burkes <shel.burkes@gmail.com>
2020-06-16 13:35:47 -0400

    Update parsify.py

--- util/parsify.py
+++ util/parsify.py
@@ -4,6 +4,7 @@ from Bio import SeqIO
 import argparse
 import numpy as np
 import pandas as pd
+import os
 
 parser = argparse.ArgumentParser(description='parsify -i <file>.fasta')
 parser.add_argument('mfilename', metavar='<filename>.fasta', type=str)
@@ -14,8 +15,17 @@ for record in SeqIO.parse(args.mfilename, "fasta"):
     listElement.append(record.id.split('#')[1])
     #print(record.seq)
     #print(sequence, file=sample)
-
 families = (set(listElement))
 for i in families:
-    print(i + "," + str(listElement.count(i)))
+    print(i + "," + str(listElement.count(i)), file=sample)
 sample.close()
+
+os.mkdir('Families')
+for i in families:
+    TE = open('Families/' + str(args.mfilename)[0:16] + '_' + str(i).replace("/","-")+'.fasta', 'a')
+#    print("Created TE family fasta file: " + i)
+    for record in SeqIO.parse(args.mfilename, "fasta"):
+        if record.id.split('#')[1] == str(i):
+            print(">" + record.id, file=TE)
+            print(record.seq, file=TE)
+    TE.close()

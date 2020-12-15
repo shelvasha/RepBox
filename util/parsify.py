a2dbe5c285a0ba771cef71d6273e7e9a7dc69258
Shel Burkes <shel.burkes@gmail.com>
2020-12-15 15:17:32 -0500

    Update parsify.py

--- util/parsify.py
+++ util/parsify.py
@@ -5,11 +5,13 @@ import argparse
 import numpy as np
 import pandas as pd
 import os
+import ntpath
 
 parser = argparse.ArgumentParser(description='parsify -i <file>.fasta')
 parser.add_argument('mfilename', metavar='<filename>.fasta', type=str)
 args = parser.parse_args()
-sample = open(args.mfilename + '.table', 'w')
+cwd = os.getcwd()
+sample = open(cwd + '/' + ntpath.basename(args.mfilename) + '.table', 'w')
 listElement = []
 for record in SeqIO.parse(args.mfilename, "fasta"):
     listElement.append(record.id.split('#')[1])
@@ -18,12 +20,13 @@ for record in SeqIO.parse(args.mfilename, "fasta"):
 families = (set(listElement))
 for i in families:
     print(i + "," + str(listElement.count(i)), file=sample)
+    #print(i + "," + str(listElement.count(i)))
 sample.close()
 
 os.mkdir('Families')
 for i in families:
-    TE = open('Families/' + str(args.mfilename)[0:16] + '_' + str(i).replace("/","-")+'.fasta', 'a')
-#    print("Created TE family fasta file: " + i)
+    TE = open(cwd + '/' + 'Families/' + str(i).replace("/","-")+'.fasta', 'a')
+    print("Created TE family fasta file: " + i)
     for record in SeqIO.parse(args.mfilename, "fasta"):
         if record.id.split('#')[1] == str(i):
             print(">" + record.id, file=TE)
